9fafe4b191862aa3a6967ccf0f5e2853ac95e126
VHarastei <60541960+VHarastei@users.noreply.github.com>
2020-11-29 19:35:01 +0200

    Added localStorage and fixed some bugs

--- app.js
+++ app.js
@@ -6,65 +6,64 @@ let area = null;
 let listsNumber = 0;
 let footer = document.createElement('div');
 footer.className = 'footer';
+let index = 0;
+
+let itemsArray = localStorage.getItem('items') ?
+JSON.parse(localStorage.getItem('items') ) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray) );
+const data = JSON.parse(localStorage.getItem('items') );
 
 form.addEventListener('submit',(e) => {
     e.preventDefault();
     let todoText = input.value;
 
     if(todoText) {
-        let todoElem = document.createElement('li');
-        todoElem.classList.add('list-item');
-        todoElem.draggable = 'true';
-
-        let elemToggle = document.createElement('div');
-        elemToggle.innerText = '✔';
-        elemToggle.classList.add('toggle');
-
-        let elemText = document.createElement('span');
-        elemText.innerText = todoText;
-        elemText.classList.add('todoText');
-        
-        let elemClose = document.createElement('div');
-        elemClose.innerText = '✖';
-        elemClose.classList.add('close');
-
-        todoElem.append(elemToggle);
-        todoElem.append(elemText);
-        todoElem.append(elemClose);
-        
-        if(todos.lastChild.className != 'footer') {
-        todos.append(footer);
-        }
-        todos.prepend(todoElem); 
-
-        listsNumber = todos.querySelectorAll('.list-item').length;
-        footer.innerText = listsNumber + ' items left'; 
-
+        itemsArray.push(todoText);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(todoText);
     }
     input.value = '';
 });
 
+data.forEach(item => {
+    liMaker(item);
+})
+
 todos.addEventListener('click', (e) => {
+    let footerText = footer.querySelector('.footer-text');
+    let textForFooter = (listsNumber == 1) ? ' item': ' items';
     let elemToggle = e.target.closest('.toggle');
     if(elemToggle) {
         completeTodo(elemToggle);
-        if(elemToggle.className == 'toggle after') {
-        footer.innerText = --listsNumber + ' items left';
-        }
-        else footer.innerText = ++listsNumber + ' items left';
     }
     
     let elemClose = e.target.closest('.close');
     if(elemClose) {
-        if(elemClose.previousSibling.previousSibling.className 
-        != 'toggle after' && elemClose) {
-            footer.innerText = --listsNumber + ' items left';
-        }
+        localStorage.clear();
+        let arrayIndex = elemClose.parentNode.getAttribute('index');
+        console.log(arrayIndex);
+        itemsArray.splice(arrayIndex, 1);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+
+        footerText.innerText = --listsNumber + textForFooter;
         closeTodo(elemClose);
     } 
 
+    let elemCloseAll = e.target.closest('.footer-clear');
+    if(elemCloseAll) {
+        let listItems = document.querySelectorAll('.list-item');
+        localStorage.clear();
+        listItems.forEach(item => {
+            item.remove();
+        })
+    }
+
     listItems = document.querySelectorAll('.list-item');
-    if(listItems.length == 0) footer.remove();
+    if(listItems.length == 0) {
+        footer.lastChild.remove();
+        footer.remove();
+    }
 });
 
 todos.addEventListener('dblclick', (e) => {
@@ -98,6 +97,54 @@ todos.addEventListener('dblclick', (e) => {
     todos.insertBefore(activeElement, nextElement);
  });
 
+function liMaker(text) {
+
+    let todoElem = document.createElement('li');
+    todoElem.classList.add('list-item');
+    todoElem.draggable = 'true';
+    todoElem.setAttribute('index', index);
+    index++;
+
+    let elemToggle = document.createElement('div');
+    elemToggle.innerText = '✔';
+    elemToggle.classList.add('toggle');
+
+    let elemText = document.createElement('span');
+    elemText.innerText = text;
+    elemText.classList.add('todoText');
+        
+    let elemClose = document.createElement('div');
+    elemClose.innerText = '✖';
+    elemClose.classList.add('close');
+
+    todoElem.append(elemToggle);
+    todoElem.append(elemText);
+    todoElem.append(elemClose);
+    todos.prepend(todoElem); 
+
+    footerMaker();
+}
+
+function footerMaker() {
+    let footerText = document.createElement('span');
+    footerText.classList.add('footer-text');
+    let footerClear = document.createElement('span');
+    footerClear.classList.add('footer-clear');
+    footerClear.innerText = 'clear all';
+
+    if(todos.lastChild.className != 'footer') {
+        footer.append(footerText);
+        footer.append(footerClear);
+         
+        todos.append(footer);
+    }
+        
+    let footerTextForm = footer.querySelector('.footer-text');
+    listsNumber = todos.querySelectorAll('.list-item').length;
+
+    let textForFooter = (listsNumber == 1) ? ' item': ' items';
+    footerTextForm.innerText = listsNumber + textForFooter; 
+}
 
 function completeTodo(elem) {
     if(!elem ||!elem.classList.contains('toggle') ) return;
@@ -110,6 +157,10 @@ function completeTodo(elem) {
     elem.classList.add('after');
     elem.nextSibling.classList.add('completed');
     }
+}
+
+function closeTodo(elem) {
+    elem.parentNode.remove();
 }
 
 function editStart(elem) {
@@ -129,19 +180,24 @@ function editStart(elem) {
 }
 
 function editEnd(elem) {
+    let footerText = footer.querySelector('.footer-text');
     elem.innerText = area.value;
     area.replaceWith(elem);
+
+    let arrayIndex = elem.parentNode.getAttribute('index');
+    itemsArray.splice(arrayIndex, 1, area.value);
+
     if(area.value == '') {
+        itemsArray.splice(arrayIndex, 1);
         closeTodo(elem);
-        if(elem.previousSibling.className != 'toggle after')
-        footer.innerText = --listsNumber + ' items left';
+        footerText.innerText = --listsNumber + ' items';
     }
     
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+
     listItems = document.querySelectorAll('.list-item');
-    if(listItems.length == 0) footer.remove();
-
-}
-
-function closeTodo(elem) {
-    elem.parentNode.remove();
+    if(listItems.length == 0) {
+        footer.lastChild.remove();
+        footer.remove();
+    }
 }
